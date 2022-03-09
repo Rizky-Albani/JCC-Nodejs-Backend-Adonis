4a8b1bf449432482a7996d04cd0c2f42f092d92c
Rizky-Albani <albanivenom123@gmail.com>
2022-03-09 23:50:48 +0700

    Function

---
+++ Tugas Hari 3 - Function Looping/Function/Lab/functions.js
@@ -0,0 +1,19 @@
+function teriak(){
+    var x = "Halo JCC!"
+    return x;
+}
+
+function kalikan(number1, number2){
+    return number1 * number2;
+}
+
+function introduce(name, age, address, hobby){
+    var combine = "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby + "!";
+    return combine;
+}
+
+module.exports = {
+    teriak: teriak,
+    kalikan: kalikan,
+    introduce: introduce,
+}
---
+++ Tugas Hari 3 - Function Looping/Function/index.js
@@ -0,0 +1,19 @@
+var x = require('./Lab/functions')
+
+var teriak = x.teriak
+var kalikan = x.kalikan
+var introduce = x.introduce
+
+var args = process.argv
+
+switch(args[2]){
+    case "teriak" : 
+        console.log(teriak())
+    break;
+    case "kalikan" :
+        console.log(kalikan(args[3], args[4]))
+    break;
+    case "introduce" : 
+        console.log(introduce(args[3], args[4], args[5], args[6]))
+    break;
+}
